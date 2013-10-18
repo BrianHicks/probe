09d3f2c362308164cc6fd949b60352a2ee5846fe
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-10-18 06:54:57 -0500

    add repr to StdoutOutput

--- probe/outputs.py
+++ probe/outputs.py
@@ -12,5 +12,8 @@ class StdoutOutput(Output):
         if self.level == 'debug':
             print 'DEBUG: Initialized StdoutOutput'
 
+    def __repr__(self):
+        return '<%s: %s>' % (self.__class__.__name__, self.level)
+
     def send(self, key, value):
         print 'INFO: sending with key %r and value %r' % (key, value)
